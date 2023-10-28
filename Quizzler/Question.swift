d758aa5718121ddb81e4cc307df0c6b879598b3a
Suraj_KY <face.surajyadav@gmail.com>
2023-10-28 20:55:04 +0530

    Structure Completed

---
+++ Quizzler/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Suraj KY on 28/10/23.
+//
+
+import Foundation
---
+++ Quizzler/QuizBrain.swift
@@ -0,0 +1,8 @@
+//
+//  QuizBrain.swift
+//  Quizzler
+//
+//  Created by Suraj KY on 28/10/23.
+//
+
+import Foundation
